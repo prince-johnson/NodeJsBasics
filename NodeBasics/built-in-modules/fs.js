0780a4defd230726f35151a16a14f6b7e83b0df9
Prince Johnson <prince.j@inapp.com>
2023-01-11 10:16:38 +0530

    fs module continueation

--- NodeBasics/built-in-modules/fs.js
+++ NodeBasics/built-in-modules/fs.js
@@ -2,8 +2,29 @@ const fs = require("fs");
 
 // blocking and non blocking
 
-const { readFileSync, writeFileSync } = fs;
-const first = readFileSync("NodeBasics\built-in-modules\fs.js", "utf-8");
-console.log(first);
+// const { readFileSync, writeFileSync } = fs;
+// const first = readFileSync("NodeBasics\built-in-modules\fs.js", "utf-8");
+// console.log(first);
 
 // writeFileSync("first.txt", "how are ou");
+
+const { readFile, writeFile } = fs;
+
+readFile("first.txt", "utf-8", (err, res)=>{
+    if (err) {
+        console.log(err);
+        return;
+    }
+    const first = res;
+    writeFile("second.txt", first, (err, res)=>{
+        if (err) {
+            console.log(err);
+        }
+        readFile("second.txt", "utf-8", (err, res)=>{
+            if(err) {
+                console.log(err);
+            }
+            console.log(res);
+        })
+    });
+});
